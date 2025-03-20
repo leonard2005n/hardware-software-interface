05fa2feef0470fd3fe83b39b0d5ff0df336feb67
Leonard <leonardneacsa2005@gmail.com>
2025-03-20 12:42:35 +0200

    Binary search-lab3
    Signed-off-by: Leonard <leonardneacsa2005@gmail.com>

--- labs/lab-03/tasks/binary_search/support/binary_search.c
+++ labs/lab-03/tasks/binary_search/support/binary_search.c
@@ -10,8 +10,21 @@ int binary_search(int *v, int len, int dest)
 	int end = len - 1;
 	int middle;
 
-	/**
-	 * TODO: Implement binary search
-	 */
+	wow:
+	if (start > end)
+		goto end;
+	middle = (start + end) / 2;
+	if (v[middle] == dest)
+		goto ok;
+	if (dest < v[middle])
+		goto statement;
+	start = middle + 1;
+	goto wow;
+	statement:
+	end = middle - 1;
+	goto wow;
+	ok:
+	return middle;
+	end:
 	return -1;
 }
